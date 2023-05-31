4cd728714ecbacd232083904f646c464be6b5add
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-05-31 10:27:10 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [dataCard, setDataCard] = useState([]);
+  const [visible, setVisible] = useState(5)
+////get data
+  const allDataCard = async () => {
+    const response = await axios.get("https://randomuser.me/api/?results=35");
+    setDataCard(response.data.results)
+  }
+///loading more data
+  const loadingMore = () => {
+    setVisible(visible + 5)
+  }
+  useEffect(() => {
+    allDataCard()
+  },[]);
+ 
+//// rendered card 
+const renderedCard = (p, index) => {
+  return(
+    <Card style={{width:"18rem",backgroundColor:"#F5F5F5", borderRadius:"10px"}} key={index}>
+      <Card.Img variant="top" src={p.picture.large} />
+      <Card.Body>
+        <Card.Title>
+          {p.name.first} {p.name.last}
+        </Card.Title>
+        <Card.Text>
+          {p.location.city}, {p.location.state}, {p.location.country}
+        </Card.Text>
+        <Card.Text>
+          <ul>
+            <li>{p.email}</li>
+            <li>{p.cell}</li>
+            <li>{p.gender}</li>
+          </ul>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="cards">
+          {dataCard.slice(0, visible).map(renderedCard)}
+        </div>
+      </div>
+      {visible < dataCard.length && (
+        <button onClick={loadingMore}>Load 5 more</button>
+      )}
     </div>
   );
 }
